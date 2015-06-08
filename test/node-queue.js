bcf2d9e7af15d9c61a5e99002eb75783f7911b26
marinewater <HartzFEAR@gmail.com>
2015-06-08 18:24:30 +0200

    updated and added tests

--- test/node-queue.js
+++ test/node-queue.js
@@ -6,48 +6,107 @@ var Queue = require('../lib/node-queue');
 describe('node-redis', function() {
     describe('enqueue', function() {
         it('should add a job to the queue', function(done) {
-            var rpushMock = {
-                rpush: function () {},
+            var redisMock = {
+                incr: function () {},
+                set: function () {},
+                zadd: function () {},
                 on: function() {}
             };
-            var mockClient = sinon.mock(rpushMock);
+            var mockClient = sinon.mock(redisMock);
 
-            mockClient.expects('rpush').once().withArgs('ncsq:test-queue:jobs', JSON.stringify('test-job')).yields(null, 'test');
+            var job_data = JSON.stringify({
+                data: {job: 'done'},
+                user_id: 567,
+                priority: 3,
+                id: 123
+            });
+
+            mockClient.expects('incr').once().withArgs('ncsq:test-queue:id').yields(null, 123);
+            mockClient.expects('set').once().withArgs('ncsq:test-queue:jobs:123', job_data).yields(null, 123);
+            mockClient.expects('zadd').once().withArgs('ncsq:test-queue:user:567', sinon.match.number, 123).yields(null, null);
+            mockClient.expects('zadd').once().withArgs('ncsq:test-queue:queue', sinon.match.number, 123).yields(null, null);
             mockClient.expects('on');
 
 
             var queue = new Queue({
-                redis_client: rpushMock
+                redis_client: redisMock
             });
-            queue.enqueue('test-queue', 'test-job').then(function() {
+            queue.enqueue('test-queue', {job: 'done'}, 567).then(function() {
                 mockClient.verify();
                 mockClient.restore();
                 done();
-            }).catch(function() {
+            }).catch(function(err) {
                 mockClient.restore();
-                done("promise should not be rejected");
+                done(err);
+            });
+        });
+        it('should add a job to the queue with different priority', function(done) {
+            var redisMock = {
+                incr: function () {},
+                set: function () {},
+                zadd: function () {},
+                on: function() {}
+            };
+            var mockClient = sinon.mock(redisMock);
+
+            var job_data = JSON.stringify({
+                data: {job: 'done'},
+                user_id: 567,
+                priority: 5,
+                id: 123
+            });
+
+            mockClient.expects('incr').once().withArgs('ncsq:test-queue:id').yields(null, 123);
+            mockClient.expects('set').once().withArgs('ncsq:test-queue:jobs:123', job_data).yields(null, 123);
+            mockClient.expects('zadd').once().withArgs('ncsq:test-queue:user:567', sinon.match.number, 123).yields(null, null);
+            mockClient.expects('zadd').once().withArgs('ncsq:test-queue:queue', sinon.match.number, 123).yields(null, null);
+            mockClient.expects('on');
+
+
+            var queue = new Queue({
+                redis_client: redisMock
+            });
+            queue.enqueue('test-queue', {job: 'done'}, 567, 5).then(function() {
+                mockClient.verify();
+                mockClient.restore();
+                done();
+            }).catch(function(err) {
+                mockClient.restore();
+                done(err);
             });
         });
 
         it('should fail to add a job to the queue', function(done) {
-            var rpushMock = {
-                rpush: function () {},
+            var redisMock = {
+                incr: function () {},
+                set: function () {},
+                zadd: function () {},
                 on: function() {}
             };
-            var mockClient = sinon.mock(rpushMock);
+            var mockClient = sinon.mock(redisMock);
 
-            mockClient.expects('rpush').once().withArgs('ncsq:test-queue:jobs', JSON.stringify('test-job')).yields('err', null);
+            var job_data = JSON.stringify({
+                data: {job: 'done'},
+                user_id: 567,
+                priority: 5,
+                id: 123
+            });
+
+            mockClient.expects('incr').once().withArgs('ncsq:test-queue:id').yields('err', null);
+            mockClient.expects('set').once().withArgs('ncsq:test-queue:jobs:123', job_data).yields(null, 123);
+            mockClient.expects('zadd').once().withArgs('ncsq:test-queue:user:567', sinon.match.number, 123).yields(null, null);
+            mockClient.expects('zadd').once().withArgs('ncsq:test-queue:queue', sinon.match.number, 123).yields(null, null);
             mockClient.expects('on');
 
 
             var queue = new Queue({
-                redis_client: rpushMock
+                redis_client: redisMock
             });
-            queue.enqueue('test-queue', 'test-job').then(function() {
-                mockClient.restore();
-                done("promise should not be resolved");
-            }).catch(function() {
+            queue.enqueue('test-queue', {job: 'done'}, 567, 5).then(function() {
                 mockClient.verify();
+                mockClient.restore();
+                done('should fail');
+            }).catch(function(err) {
                 mockClient.restore();
                 done();
             });
@@ -56,95 +115,99 @@ describe('node-redis', function() {
 
     describe('dequeue', function() {
         it('should pop a job from the queue', function (done) {
-            var multiMock = {
-                lrange: function () {},
-                ltrim: function () {},
+            var multiMock1 = {
+                zrange: function () {},
+                zremrangebyrank: function () {},
+                mget: function () {},
+                del: function () {},
                 exec: function() {}
             };
-            var mockClient1 = sinon.mock(multiMock);
+            var mockClient1 = sinon.mock(multiMock1);
 
-            mockClient1.expects('lrange').once().withArgs('ncsq:test-queue:jobs', 0);
-            mockClient1.expects('ltrim').once().withArgs('ncsq:test-queue:jobs', 1, -1);
-            mockClient1.expects('exec').once().yields(null, [[JSON.stringify('test')], 'OK']);
+            mockClient1.expects('zrange').once().withArgs('ncsq:test-queue:queue', 0, 4);
+            mockClient1.expects('zremrangebyrank').once().withArgs('ncsq:test-queue:queue', 0, 4);
+            mockClient1.expects('exec').once().yields(null, [[3, 5], 'OK']);
+
+
+            var multiMock2 = {
+                mget: function () {},
+                del: function () {},
+                exec: function() {}
+            };
+            var mockClient3 = sinon.mock(multiMock2);
+
+            mockClient3.expects('mget').once().withArgs([
+                'ncsq:test-queue:jobs:3',
+                'ncsq:test-queue:jobs:5'
+            ]);
+            mockClient3.expects('del').once().withArgs([
+                'ncsq:test-queue:jobs:3',
+                'ncsq:test-queue:jobs:5'
+            ]);
+
+            mockClient3.expects('exec').once().yields(null, [[
+                JSON.stringify({
+                    data: {job: 'done1'},
+                    user_id: 567,
+                    priority: 3,
+                    id: 3
+                }),
+                JSON.stringify({
+                    data: {job: 'done2'},
+                    user_id: 890,
+                    priority: 4,
+                    id: 5
+                })
+            ], 'OK']);
 
 
             var redisMock = {
-                multi: function() { return multiMock; },
+                multi: function() {},
+                zrem: function() {},
                 on: function() {}
             };
 
             var mockClient2 = sinon.mock(redisMock);
+
+            var multi = mockClient2.expects('multi').twice().onCall(0).returns(multiMock1).onCall(1).returns(multiMock2);
             mockClient2.expects('on');
+            mockClient2.expects('zrem').once().withArgs('ncsq:test-queue:user:567', 3);
+            mockClient2.expects('zrem').once().withArgs('ncsq:test-queue:user:890', 5);
 
 
             var queue = new Queue({
                 redis_client: redisMock
             });
-            queue.dequeue('test-queue', 'test-job').then(function (reply) {
+
+            queue.dequeue('test-queue', 5).then(function (reply) {
                 mockClient1.verify();
                 mockClient1.restore();
                 mockClient2.restore();
-                assert.equal(reply[0], 'test');
+                assert.deepEqual(reply[0], {job: 'done1'});
+                assert.deepEqual(reply[1], {job: 'done2'});
                 done();
-            }).catch(function () {
+            }).catch(function (err) {
                 mockClient1.restore();
                 mockClient2.restore();
-                done("promise should not be rejected");
-            });
-        });
-
-        it('should fail to pop a job from the queue', function (done) {
-            var multiMock = {
-                lrange: function () {},
-                ltrim: function () {},
-                exec: function() {}
-            };
-            var mockClient1 = sinon.mock(multiMock);
-
-            mockClient1.expects('lrange').once().withArgs('ncsq:test-queue:jobs', 0);
-            mockClient1.expects('ltrim').once().withArgs('ncsq:test-queue:jobs', 1, -1);
-            mockClient1.expects('exec').once().yields('err', null);
-
-
-            var redisMock = {
-                multi: function() { return multiMock; },
-                on: function() {}
-            };
-
-            var mockClient2 = sinon.mock(redisMock);
-            mockClient2.expects('on');
-
-
-            var queue = new Queue({
-                redis_client: redisMock
-            });
-            queue.dequeue('test-queue', 'test-job').then(function (reply) {
-                mockClient1.verify();
-                mockClient1.restore();
-                mockClient2.restore();
-                done("promise should not be resolved");
-            }).catch(function () {
-                mockClient1.restore();
-                mockClient2.restore();
-                done();
+                done(err);
             });
         });
     });
 
     describe('countJobs', function() {
         it('should return the number of jobs in the queue', function(done) {
-            var llenMock = {
-                llen: function () {},
+            var zcountMock = {
+                zcount: function () {},
                 on: function() {}
             };
-            var mockClient = sinon.mock(llenMock);
+            var mockClient = sinon.mock(zcountMock);
 
-            mockClient.expects('llen').once().withArgs('ncsq:test-queue:jobs').yields(null, 2);
+            mockClient.expects('zcount').once().withArgs('ncsq:test-queue:queue', '-inf', '+inf').yields(null, 2);
             mockClient.expects('on');
 
 
             var queue = new Queue({
-                redis_client: llenMock
+                redis_client: zcountMock
             });
             queue.countJobs('test-queue').then(function(job_count) {
                 assert.deepEqual(job_count, 2);
@@ -156,6 +219,36 @@ describe('node-redis', function() {
             }).catch(function() {
                 mockClient.restore();
                 done("promise should not be rejected");
+            });
+        });
+    });
+
+    describe('getPosition', function() {
+        it('get the first position of a job for a user id', function(done) {
+            var redisMock = {
+                zrange: function () {},
+                zrank: function () {},
+                on: function() {}
+            };
+            var mockClient = sinon.mock(redisMock);
+
+            mockClient.expects('zrange').once().withArgs('ncsq:test-queue:user:123', 0, 0).yields(null, [2]);
+            mockClient.expects('zrank').once().withArgs('ncsq:test-queue:queue', 2).yields(null, 5);
+            mockClient.expects('on');
+
+            var queue = new Queue({
+                redis_client: redisMock
+            });
+            queue.getPosition('test-queue', 123).then(function(position) {
+                assert.deepEqual(position, 6);
+
+                mockClient.verify();
+                mockClient.restore();
+
+                done();
+            }).catch(function(err) {
+                mockClient.restore();
+                done(err);
             });
         });
     });
